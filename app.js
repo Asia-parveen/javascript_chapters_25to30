b45ba0b63387b33853363da66c739bf27f57ec34
Asia-parveen <beautyhealthytips@gmail.com>
2024-07-02 05:28:23 -0700

    added all files abd folders

---
+++ app.js
@@ -0,0 +1,117 @@
+// Assignment # 26-30
+// JAVASCRIPT
+// MATH METHODS
+
+// 1. Write a program that takes a positive integer from user &
+// display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+// let num = +prompt("Enter any number");
+// console.log(`number: ${num}`);
+// console.log("Round of value:" + Math.round(num));
+// console.log("Floor of value:" + Math.floor(num));
+// console.log("ceil of value:" + Math.ceil(num));
+
+// 2. Write a program that takes a negative floating point
+// number from user & display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+// let userInput = prompt("Enter any negative floating number");
+// document.write(`number: ${userInput } <br>`);
+// let roundValue = Math.round(userInput);
+// document.write(`rounnd off value: ${roundValue} <br>`);
+// let florValue = Math.floor(Math.round(userInput));
+// document.write(`floor value: ${florValue} <br>`);
+// let celValue = Math.ceil(userInput);
+// document.write(`ceil value: ${celValue} <br>`);
+
+// 4. Write a program that simulates a dice using random()
+// method of JS Math class. Display the value of dice in your
+// browser.:
+
+// let randomValue = Math.floor(Math.random()*7);
+// document.write(`random dice value: ${randomValue}<br>`);
+// let randomValue1 = Math.floor(Math.random()*7);
+// document.write(`random dice value: ${randomValue1}`);
+
+// 5. Write a program that simulates a coin toss using random()
+// method of JS Math class. Display the value of coin in your
+// browser
+
+// let randomNum = Math.floor(Math.random()*2+1);
+// let user1="Head";
+// let user2="Tail";
+
+// if(randomNum === 2){
+//     let user1="Head";
+//     document.write(`${randomNum}<br> random coin value: ${user1}`);
+// }else{
+//     let user2="Tail";
+//     document.write(`${randomNum}<br> random coin value: ${user2}`);
+// };
+
+// 6. Write a program that shows a random number between 1
+// and 100 in your browser.
+
+// let randomNum = Math.floor(Math.random()*101);
+// document.write("Random number between 1 and 100 : " + randomNum );
+   
+// 7. Write a program that asks the user about his weight. Parse
+// the user input and display his weight in your browser.
+// Possible user inputs can be:
+// a. 50
+// b. 50kgs
+// c. 50.2kgs
+// d. 50.2kilograms
+
+// let userWeight = +prompt("Enter your weight");
+// document.write(`The weight of user is ${ userWeight} kilograms`);
+
+// 8. Write a program that stores a random secret number from
+// 1 to 10 in a variable. Ask the user to input a number
+// between 1 and 10. If the user input equals the secret
+// number, congratulate the user.
+
+
+// let userNum = +prompt("Enter a number 0 t0 10");
+// let mySecNum = Math.floor(Math.random()*11);
+// if(userNum === mySecNum){
+//     console.log(`congrazulation`);
+// }else{
+//     console.log("try again");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
